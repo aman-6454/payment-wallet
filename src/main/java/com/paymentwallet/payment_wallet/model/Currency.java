34ebc6200e2bf843975990e362790d4acb971e5c
RAI <aman.rai@capgemini.com>
2025-07-18 14:48:34 +0530

    adding lombok annotation

--- src/main/java/com/paymentwallet/payment_wallet/model/Currency.java
+++ src/main/java/com/paymentwallet/payment_wallet/model/Currency.java
@@ -1,10 +1,14 @@
 package com.paymentwallet.payment_wallet.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Currency {
 
 
@@ -18,46 +22,11 @@ public class Currency {
     @OneToMany(mappedBy = "currency")
     private List<Wallet> wallets;
 
-
-
-
     public Currency() {
         super();
     }
 
     public Currency(String abbreviation) {
         this.abbreviation=abbreviation;
-    }
-
-    public int getCurrencyId() {
-        return currencyId;
-    }
-
-    public void setCurrencyId(int currencyId) {
-        this.currencyId = currencyId;
-    }
-
-    public List<Wallet> getWallets() {
-        return wallets;
-    }
-
-    public void setWallets(List<Wallet> wallets) {
-        this.wallets = wallets;
-    }
-
-    public String getAbbreviation() {
-        return abbreviation;
-    }
-
-    public void setAbbreviation(String abbreviation) {
-        this.abbreviation = abbreviation;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
