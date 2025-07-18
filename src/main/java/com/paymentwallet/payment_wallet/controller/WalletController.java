4be923cdb3f3aa568dc10a392b50302ec9fa2f55
RAI <aman.rai@capgemini.com>
2025-07-18 14:13:29 +0530

    fixed sonar v2

--- src/main/java/com/paymentwallet/payment_wallet/controller/WalletController.java
+++ src/main/java/com/paymentwallet/payment_wallet/controller/WalletController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import com.paymentwallet.payment_wallet.model.TransferDTO;
 import com.paymentwallet.payment_wallet.model.UserDTO;
 import com.paymentwallet.payment_wallet.service.WalletService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
